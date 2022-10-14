8b16b3639fad55d3da3acca6f46812e1d0cdae78
humbertoluks <luks.dev@outlook.com>
2022-10-14 09:18:15 -0300

    add login form

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -11,19 +11,22 @@ export interface TextProps {
   size?: 'sm' | 'md' | 'lg';
   children: ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 
-export function Text({ size = 'md', children, asChild }: TextProps) {
+export function Text({ size = 'md', children, asChild, className }: TextProps) {
   const Comp = asChild ? Slot : 'span';
 
   return (
     <Comp
       className={clsx(
-        ' text-gray-100 font-sans', {
-        'text-xs': size === 'sm',
-        'text-sm': size === 'md',
-        'text-lg': size === 'lg',
-      }
+        ' text-gray-100 font-sans',
+        {
+          'text-xs': size === 'sm',
+          'text-sm': size === 'md',
+          'text-lg': size === 'lg',
+        },
+        className
       )}
     >
       {children}
